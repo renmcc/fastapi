181eb9218215a363e8d41c8777707961bb060305
ren_mcc <ren_mcc@foxmail.com>
2021-02-09 17:00:39 +0800

    """Dependencies 创建、导入和声明依赖"""

--- tutonal/chapter05.py
+++ tutonal/chapter05.py
@@ -1,3 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from typing import Optional
 
 app05 = APIRouter()
+"""Dependencies 创建、导入和声明依赖"""
+
+
+async def common_parameters(q: Optional[str] = None,
+                            page: int = 1,
+                            limit: int = 10):
+    return {"q": q, "page": page, "limit": limit}
+
+
+@app05.get("/dependency01")
+async def dependency01(commons: dict = Depends(common_parameters)):
+    return commons
+
+
+@app05.get("/dependency02")
+def dependency02(commons: dict = Depends(common_parameters)):
+    return commons
