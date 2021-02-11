85f64c92652f8b3e23f7e21584043d159dc6207a
ren_mcc <ren_mcc@foxmail.com>
2021-02-12 01:27:58 +0800

    """Bigger Applications - Multiple Files 多应用的目录结构设计"""

--- tutonal/chapter07.py
+++ tutonal/chapter07.py
@@ -1,3 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Depends
+"""Bigger Applications - Multiple Files 多应用的目录结构设计"""
 
-app07 = APIRouter()
+
+async def get_user_agent(request: Request):
+    print(request.headers["User-Agent"])
+
+
+app07 = APIRouter(prefix="/bigger_applications",
+                  tags=["第七章 FastAPI的数据库操作和多应用的目录结构设计"],
+                  dependencies=[Depends(get_user_agent)],
+                  responses={200: {
+                      "description": "Good job!"
+                  }})
+
+
+@app07.get("/bigger_application")
+async def bigger_application():
+    return {"message": "Bigger Application"}
