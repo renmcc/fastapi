49564b79c357a32a63622c1ff91c74540c989fc2
ren_mcc <ren_mcc@foxmail.com>
2021-02-11 14:50:22 +0800

    创建和数据表对应的序列化模型

---
+++ coronavirus/schemas.py
@@ -0,0 +1,36 @@
+from date3time import datetime
+from datetime import date as date_
+from pydantic import BaseModel
+
+
+class CreateData(BaseModel):
+    date: date_
+    confirmed: int = 0
+    deaths: int = 0
+    recovered: int = 0
+
+
+class CreateCity(BaseModel):
+    province: str
+    country: str
+    country_code: str
+    country_population: int
+
+
+class ReadData(CreateData):
+    id: int
+    city_id: int
+    updated_at: datetime
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class ReadCity(CreateCity):
+    id: int
+    updated_at: datetime
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
